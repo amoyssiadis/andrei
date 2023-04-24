f88ff78dd7ff8f3895fb82d8251dd58856c2f164
Eric Milfont <eric@milfont.net>
2023-04-24 00:30:21 -0300

    added logo images width normalization

--- slices/LogoMarquee/index.js
+++ slices/LogoMarquee/index.js
@@ -1,12 +1,42 @@
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Marquee from 'react-fast-marquee'
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
 
 const LogoMarquee = ({ slice }) => {
   const [direction, setDirection] = useState('left')
   const [speed, setSpeed] = useState(slice.primary.speed || 40)
-  // console.log(slice?.items)
+
+  function adjustImageWidth(image) {
+    var widthBase = 50
+    var scaleFactor = 0.525
+    var imageRatio = image.width / image.height
+
+    image.style.width = Math.pow(imageRatio, scaleFactor) * widthBase + 'px'
+  }
+
+  useEffect(() => {
+    const images = document.querySelectorAll('.marquee-image')
+
+    function handleImageLoad() {
+      adjustImageWidth(this)
+    }
+
+    images.forEach((image) => {
+      if (image.complete) {
+        adjustImageWidth(image)
+      } else {
+        image.addEventListener('load', handleImageLoad)
+      }
+    })
+
+    return () => {
+      images.forEach((image) => {
+        image.removeEventListener('load', handleImageLoad)
+      })
+    }
+  }, [])
+
   return (
     <section className="mb-4 mt-12  p-1 sm:mt-24 ">
       <p className="my-4 text-center text-lg font-bold tracking-wider sm:text-lg ">
@@ -26,17 +56,11 @@ const LogoMarquee = ({ slice }) => {
         >
           {slice?.items?.map((item, i) => (
             <div key={i} className=" mr-6 ">
-              <Link href={item.link.url || '/#'}>
-                <a>
-                  <img
-                    src={item.image.url}
-                    alt={item.image.alt}
-                    width="100%"
-                    height="100%"
-                    className=" max-h-14 object-cover	object-center"
-                  />
-                </a>
-              </Link>
+              <img
+                src={item.image.url}
+                alt={item.image.alt}
+                className="marquee-image max-h-14  object-cover	object-center"
+              />
             </div>
           ))}
         </Marquee>
