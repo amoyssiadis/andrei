927fc36ba45c09da46931810e8160b2be9309541
Eric Milfont <eric@milfont.net>
2023-05-04 11:11:36 -0300

    Footer margin + longe do bottom

--- slices/Footer/index.js
+++ slices/Footer/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Footer = ({ slice }) => (
-  <section className="mt-10  pb-4 text-center">
+  <section className="text-center">
     <div>
       &#169; Andrei Moyssiadis 2023 <br />
       All rights reserved
