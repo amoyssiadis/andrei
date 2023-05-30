07fee787ba8a3a4d91701dcd9a8e54d9df8e673e
Eric Milfont <eric@milfont.net>
2023-05-30 00:26:17 -0300

    404: adicionar padding lateral

--- pages/404.js
+++ pages/404.js
@@ -6,15 +6,15 @@ export default function FourOhFour() {
   return (
     <>
       <GrainedEffect>
-        <div className="flex flex-col items-center justify-center   ">
+        <div className="flex flex-col items-center justify-center    ">
           <div className="flex h-screen w-screen  items-center justify-center bg-[url('/logobg.webp')]   text-center  ">
             <Link href="/">
               <a>
-                <div className="flex flex-col items-center justify-center ">
+                <div className="flex flex-col items-center justify-center px-6 ">
                   <img
                     alt="Page not Found"
                     src="/404.svg"
-                    className="w-[900px]"
+                    className="w-full max-w-[900px]"
                   />
                   <img
                     alt="Click to go back"
