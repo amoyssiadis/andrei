f816522c11f3bdce58092341da61db9ae0af88ca
Eric Milfont <eric@milfont.net>
2023-08-28 10:01:57 -0300

    Update ImageAnim.js

--- components/ImageAnim.js
+++ components/ImageAnim.js
@@ -9,24 +9,28 @@ function ImageAnim(images) {
   }
   return (
     <div className="">
-      <div className="x pointer-events-auto absolute w-28 cursor-pointer sm:w-52	 ">
-        <img
-          className="y"
-          src={images.images[0].image.url}
-          key="1"
-          alt="Close / Fechar"
-          onClick={closeImage}
-        />
-      </div>
-      <div className="  x2 pointer-events-auto absolute right-0 top-0  w-28 cursor-pointer sm:w-52	">
-        <img
-          className="y2"
-          src={images.images[1].image.url}
-          key="2"
-          alt="Close / Fechar"
-          onClick={closeImage}
-        />
-      </div>
+      {images.images[0] && (
+        <div className="x pointer-events-auto absolute w-28 cursor-pointer sm:w-52	 ">
+          <img
+            className="y"
+            src={images.images[0].image.url}
+            key="1"
+            alt="Close / Fechar"
+            onClick={closeImage}
+          />
+        </div>
+      )}
+      {images.images[1] && (
+        <div className="  x2 pointer-events-auto absolute right-0 top-0  w-28 cursor-pointer sm:w-52	">
+          <img
+            className="y2"
+            src={images.images[1].image.url}
+            key="2"
+            alt="Close / Fechar"
+            onClick={closeImage}
+          />
+        </div>
+      )}
     </div>
   )
 }
