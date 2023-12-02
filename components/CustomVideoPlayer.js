38e8a849e17168b1d5f3b7cf1cc42d5bd1ac2d59
Eric Milfont <eric@milfont.net>
2023-12-02 15:22:57 -0300

    Update CustomVideoPlayer.js
    fixed bug in safari

--- components/CustomVideoPlayer.js
+++ components/CustomVideoPlayer.js
@@ -12,6 +12,10 @@ export const CustomVideoPlayer = ({ url }) => {
   const backgroundVideo =
     url ||
     'https://andrei-portfolio.cdn.prismic.io/andrei-portfolio/ce1c0eb0-544a-4999-b90b-036ed15337fe_BJORK++DIGITAL-+MAKING+OF+FILTROS+DE+INSTAGRAM.webm'
+  // Determine the video type based on the file extension
+  const videoType =
+    backgroundVideo.split('.').pop() === 'mp4' ? 'video/mp4' : 'video/webm'
+
   return (
     <video
       ref={videoRef}
@@ -26,7 +30,7 @@ export const CustomVideoPlayer = ({ url }) => {
         top: 0,
       }}
     >
-      <source src={backgroundVideo} type="video/mp4" />
+      <source src={backgroundVideo} type={videoType} />
     </video>
   )
 }
