7ee4c4717a5ec8157dc7fda7436b52ecaeb7065d
usman.naeem <usman.naeem@pursue.today>
2024-06-10 02:29:38 +0500

    Improvements: Reverted Mansory styles & disabled auth

--- middleware.js
+++ middleware.js
@@ -5,15 +5,15 @@ export const config = {
 }
 
 export function middleware(req) {
-  const basicAuth = req.headers.get('authorization')
-  const url = req.nextUrl
-  if (basicAuth) {
-    const authValue = basicAuth.split(' ')[1]
-    const [user, pwd] = atob(authValue).split(':')
-    if (user === 'admin' && pwd === 'porto99@') {
-      return NextResponse.next()
-    }
-  }
-  url.pathname = '/api/auth'
-  return NextResponse.rewrite(url)
+  // const basicAuth = req.headers.get('authorization')
+  // const url = req.nextUrl
+  // if (basicAuth) {
+  //   const authValue = basicAuth.split(' ')[1]
+  //   const [user, pwd] = atob(authValue).split(':')
+  //   if (user === 'admin' && pwd === 'porto99@') {
+  //     return NextResponse.next()
+  //   }
+  // }
+  // url.pathname = '/api/auth'
+  // return NextResponse.rewrite(url)
 }
--- slices/Mansory/StyledMansory.js
+++ slices/Mansory/StyledMansory.js
@@ -2,61 +2,37 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import LazyLoad from 'react-lazy-load'
-import { Gallery } from 'react-grid-gallery'
 
 const StyledMansory = ({ slice, openMedia, hoverImage }) => {
-  const images = slice?.items?.map((item) => {
-    const width = item?.image?.dimensions?.width || 850
-    const height = item?.image?.dimensions?.height || 400
-
-    return {
-      src: item?.image?.url,
-      width: width,
-      height: height,
-      alt: item?.image?.alt,
-      item,
-    }
-  })
 
   return (
-    <div className="mx-auto min-h-screen max-w-6xl px-4 md:px-20">
-      <Gallery
-        images={images}
-        enableImageSelection={false}
-        thumbnailImageComponent={(props) => (
-          <CustomImage
-            props={props}
-            hoverImage={hoverImage}
-            openMedia={openMedia}
-          />
-        )}
-      />
+    <div className={'masonry px-4 md:px-20'}>
+      {slice?.items?.map((item, i) => {
+        const width = item.image.dimensions.width || 850
+        const height = item.image.dimensions.height || 400
+
+        return (
+          <div className="cursor-pointer p-1" key={i}>
+            <a onClick={() => openMedia(item)}>
+              <LazyLoad>
+                <img
+                  className=""
+                  src={item.image.url}
+                  alt={item.image.alt}
+                  width={width}
+                  height={height}
+                  onMouseOver={(e) =>
+                    hoverImage(e, item.image_hover.url || null)
+                  }
+                  onMouseOut={(e) => hoverImage(e, item.image.url)}
+                />
+              </LazyLoad>
+            </a>
+          </div>
+        )
+      })}
     </div>
   )
 }
 
 export default StyledMansory
-
-const CustomImage = ({ props, hoverImage, openMedia }) => {
-  const item = props?.item?.item
-  const is_clickable = item?.is_clickable || false
-  const url = item?.image?.url || null
-  const hover_url = item.image_hover.url || null
-  return (
-    <div
-      style={{
-        cursor: is_clickable ? 'pointer' : 'default',
-      }}
-    >
-      <a onClick={is_clickable ? () => openMedia(item) : undefined}>
-        <LazyLoad>
-        <img
-          {...props.imageProps}
-          onMouseOver={(e) => hoverImage(e, hover_url)}
-          onMouseOut={(e) => hoverImage(e, url)}
-        />
-        </LazyLoad>
-      </a>
-    </div>
-  )
-}
